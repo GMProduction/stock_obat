3153e236fc22a548ad258fc62c68f59cbd4a21fe
bagusyanuar <50134851+bagusyanuar@users.noreply.github.com>
2023-01-03 15:24:28 +0700

    migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
+        'role',
     ];
 
     /**
@@ -41,4 +41,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
 }
