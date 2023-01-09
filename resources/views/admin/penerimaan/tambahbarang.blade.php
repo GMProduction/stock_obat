86b70a154ba5c57898f144f413b7b3cfcf2fb55c
pradana <pradanamahendra@gmail.com>
2023-01-09 16:40:55 +0700

    lanjut dirumah

--- resources/views/admin/penerimaan/tambahbarang.blade.php
+++ resources/views/admin/penerimaan/tambahbarang.blade.php
@@ -63,12 +63,25 @@
                         </div>
 
                         <div class="mb-3 mt-5">
-                            <label for="nomor-batch" class="block mb-2 text-sm font-medium text-gray-700 mt-3">Tanggal Diterima
+                            <label for="nomor-batch" class="block mb-2 text-sm font-medium text-gray-700 mt-3">Tanggal
+                                Diterima
                             </label>
-                            <input type="text" id="e-nama-info"
-                                class="bg-gray-50 border  w-full border-gray-300 text-gray-900 text-sm  block  p-2.5 "
-                                placeholder="Nomor Batch" readonly name="nomor-batch">
+                            <div class="relative">
+                                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                            d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                                            clip-rule="evenodd"></path>
+                                    </svg>
+                                </div>
+                                <input datepicker datepicker-autohide datepicker-format="dd MM yyyy" type="text" required
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  "
+                                    placeholder="Pilih Tanggal" >
+                            </div>
                         </div>
+
+
                     </div>
                     <div class="border rounded-md col-span-2 p-3 relative">
                         <p class="text-gray-500">Barang yang diterima</p>
@@ -338,7 +351,7 @@
 
 @section('morejs')
     <!--Datatables -->
-
+    <script src="{{ asset('js/datepicker.js') }}"></script>
 
 
     {{-- MODAL MASTER --}}
