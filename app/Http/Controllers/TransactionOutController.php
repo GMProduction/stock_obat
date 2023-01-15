8735549124339a159e6d2877f95a49cb74a906f3
bagusyanuar <bagus.yanuar613@gmail.com>
2023-01-15 16:56:28 +0700

    medicine out

--- app/Http/Controllers/TransactionOutController.php
+++ app/Http/Controllers/TransactionOutController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 
 
 use App\Helper\CustomController;
+use App\Repository\MedicineOutRepository;
 
 class TransactionOutController extends CustomController
 {
-    public function __construct()
+    private $medicineOutRepository;
+
+    public function __construct(MedicineOutRepository $medicineOutRepository)
     {
         parent::__construct();
+        $this->medicineOutRepository = $medicineOutRepository;
     }
 
     public function store_cart()
@@ -19,7 +23,7 @@ class TransactionOutController extends CustomController
             $medicine_id = $this->postField('medicine');
 
             return $this->jsonResponse('success', 200);
-        }catch (\Exception $e) {
+        } catch (\Exception $e) {
             return $this->jsonResponse($e->getMessage(), 500);
         }
     }
