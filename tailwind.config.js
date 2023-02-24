ef142ff4ab476d09612cd25cfef0bc537af50dd4
pradana <pradanamahendra@gmail.com>
2023-02-24 12:07:39 +0700

    penyesuaian stock

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
     colors: {
       primary: "#29bac7",
       secondary: "#FF5A1F",
+      secondarylight1: "#FF5A1F77",
       primarylight: "#29bac777",
       primarylight2: "#29bac722",
     },
